c6d627fe4b6a166540e9ae7debea9e83cf6bd773
yjama <ab1239189@gmail.com>
2022-11-17 14:29:23 -0500

    Minor change again

--- booksapigraphql/src/main/kotlin/seg3x02/booksapigraphql/BooksapigraphqlApplication.kt
+++ booksapigraphql/src/main/kotlin/seg3x02/booksapigraphql/BooksapigraphqlApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class BooksapigraphqlApplication
-//yasinJ
+//yasinJaamac
 fun main(args: Array<String>) {
 	runApplication<BooksapigraphqlApplication>(*args)
 }
